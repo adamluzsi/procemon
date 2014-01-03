d97c4dda1120c1b3e057b183b42806ff3e393b70
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-01-03 16:26:51 +0100

    update method file

--- lib/procemon/function/proc_source/method.rb
+++ lib/procemon/function/proc_source/method.rb
@@ -30,7 +30,6 @@ module MethodToProcSource
         args_to_replace = return_string.split("\n")[0].match(/\s*def\s*\S*\s*(.*)/).captures[0]
 
         if args_to_replace != String.new
-          args_to_replace.gsub!(' ','')
           return_string.sub!(args_to_replace,"|#{args_to_replace}|")
         end
 
