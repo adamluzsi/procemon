ad5e9d0389e4f26374e3735976ca27aac034b1bc
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-03-20 05:24:55 +0100

    update file create

--- lib/procemon/function/file.rb
+++ lib/procemon/function/file.rb
@@ -4,8 +4,8 @@ class File
   def self.create(route_name ,filemod="w",string_data= ::String.new )
 
     #file_name generate
-    if !route_name.to_s.split(self.class::SEPARATOR).last.nil? || route_name.to_s.split(self.class::SEPARATOR).last != ''
-      file_name = route_name.to_s.split(self.class::SEPARATOR).last
+    if !route_name.to_s.split(File::SEPARATOR).last.nil? || route_name.to_s.split(File::SEPARATOR).last != ''
+      file_name = route_name.to_s.split(File::SEPARATOR).last
     else
       file_name = nil?
     end
@@ -13,19 +13,19 @@ class File
     #path_way
     begin
       raise ::ArgumentError, "missing route_name: #{route_name}"   if route_name.nil?
-      path = self.class.expand_path(route_name).to_s.split(self.class::SEPARATOR)
-      path = path - [self.class.expand_path(route_name).to_s.split(self.class::SEPARATOR).last]
+      path = File.expand_path(route_name).to_s.split(File::SEPARATOR)
+      path = path - [File.expand_path(route_name).to_s.split(File::SEPARATOR).last]
       path.shift
     end
 
     #job
     begin
-      if !::Dir.exists?(self.class::SEPARATOR+path.join(self.class::SEPARATOR))
+      if !::Dir.exists?(File::SEPARATOR+path.join(File::SEPARATOR))
 
-        at_now = self.class::SEPARATOR
+        at_now = File::SEPARATOR
         path.each do |dir_to_be_checked|
 
-          at_now += "#{dir_to_be_checked+self.class::SEPARATOR}"
+          at_now += "#{dir_to_be_checked+File::SEPARATOR}"
           ::Dir.mkdir(at_now) if !::Dir.exists?(at_now)
 
         end
@@ -34,11 +34,11 @@ class File
 
     # write data
     begin
-      full_path = "#{self.class::SEPARATOR+path.join(self.class::SEPARATOR)+self.class::SEPARATOR}#{file_name}"
-      if self.class.exist? full_path
-        self.class.open(full_path,filemod).write string_data
+      full_path = "#{File::SEPARATOR+path.join(File::SEPARATOR)+File::SEPARATOR}#{file_name}"
+      if File.exist? full_path
+        File.open(full_path,filemod).write string_data
       else
-        self.class.new(full_path,filemod).write string_data
+        File.new(full_path,filemod).write string_data
       end
     end
 
